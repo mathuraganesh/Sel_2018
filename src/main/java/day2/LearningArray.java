5223f4f915d0e17b903467fc1cc5804661b878c9
Mathuraveeraganesh Meenakshi Sundaram <mveeraga@in.ibm.com>
2018-08-04 17:41:34 +0530

    Base Code Learning array

---
+++ src/main/java/day2/LearningArray.java
@@ -0,0 +1,24 @@
+package day2;
+
+import java.util.Scanner;
+
+public class LearningArray {
+
+	
+		public static void main(String[] args) {
+			
+			String[] studentList= {"mahesh","Ganesh","Suresh"};
+			int[] ageList= {23,25,27};
+			char[][] gradesList= {{'A','B','C','D','E','F'},{'B','B','B','B','B','B'},{'F','F','F','F','F','F'}};
+			Scanner scr=new Scanner(System.in);
+			System.out.println("Enter the Index");
+			int aIndex=scr.nextInt();
+			aIndex=aIndex-1;
+			System.out.println(studentList[aIndex]
+			+" Whose is "+ageList[aIndex]
+			+" Years Old Scored "+gradesList[aIndex][0]+","+gradesList[aIndex][1]+","+gradesList[aIndex][2]+","+gradesList[aIndex][3]+","+gradesList[aIndex][4]+","+gradesList[aIndex][5]);
+	
+
+	}
+
+}
